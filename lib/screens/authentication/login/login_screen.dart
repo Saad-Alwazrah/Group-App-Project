028368bd389ef6593bd64a8ff4c3243f21db7f08
Solaiman AlDokhail <ssaldokhail@gmail.com>
2025-06-22 06:08:58 +0300

    dd

--- lib/screens/authentication/login/login_screen.dart
+++ lib/screens/authentication/login/login_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:june15th/screens/authentication/bloc/auth_bloc.dart';
 import 'package:june15th/screens/authentication/register/register_screen.dart';
+import 'package:june15th/screens/navbar/nav_bar.dart';
 import 'package:june15th/widgets/auth/custom_text.dart';
 import 'package:june15th/widgets/auth/submit_button.dart';
 import 'package:june15th/widgets/auth/textfields/auth_textfield_password.dart';
@@ -19,61 +20,77 @@ class LoginScreen extends StatelessWidget {
       child: Builder(
         builder: (context) {
           final bloc = context.read<AuthBloc>();
-          return Scaffold(
-            body: SafeArea(
-              child: Column(
-                children: [
-                  SizedBox(height: 32),
+          return BlocListener<AuthBloc, AuthState>(
+            listener: (context, state) {
+              if (state is SuccessState) {
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text("Login Successful!")));
+                Navigator.of(context).pushReplacement(
+                  MaterialPageRoute(builder: (context) => NavBar()),
+                );
+              } else if (state is ErrorState) {
+                ScaffoldMessenger.of(
+                  context,
+                ).showSnackBar(SnackBar(content: Text("Login Failed:")));
+              }
+            },
+            child: Scaffold(
+              body: SafeArea(
+                child: Column(
+                  children: [
+                    SizedBox(height: 32),
 
-                  ScreenLabel(labelText: "Login"),
+                    ScreenLabel(labelText: "Login"),
 
-                  SizedBox(height: 16),
+                    SizedBox(height: 16),
 
-                  BlocBuilder<AuthBloc, AuthState>(
-                    builder: (context, state) {
-                      return Form(
-                        key: bloc.loginformKey,
-                        child: Column(
-                          children: [
-                            MainLabel(labelText: "Username"),
-                            AuthTextFieldUsername(
-                              controller: bloc.usernameController!,
-                              textFieldHintText: "Enter Username",
-                              validationMethod: (value) {
-                                return bloc.usernameValidation(text: value);
-                              },
-                            ),
+                    BlocBuilder<AuthBloc, AuthState>(
+                      builder: (context, state) {
+                        return Form(
+                          key: bloc.loginformKey,
+                          child: Column(
+                            children: [
+                              MainLabel(labelText: "Username"),
+                              AuthTextFieldUsername(
+                                controller: bloc.usernameController!,
+                                textFieldHintText: "Enter Username",
+                                validationMethod: (value) {
+                                  return bloc.usernameValidation(text: value);
+                                },
+                              ),
 
-                            MainLabel(labelText: "Password"),
-                            AuthTextFieldPassword(
-                              controller: bloc.passwordController!,
-                              textFieldHintText: "Enter Password",
-                              validationMethod: (value) {
-                                return bloc.passwordValidation(text: value);
-                              },
-                            ),
-                          ],
-                        ),
-                      );
-                    },
-                  ),
-                  CustomText(
-                    firstText: "Don't have an account? ",
-                    secondText: " Reigster",
-                    destination: RegisterScreen(),
-                  ),
+                              MainLabel(labelText: "Password"),
+                              AuthTextFieldPassword(
+                                controller: bloc.passwordController!,
+                                textFieldHintText: "Enter Password",
+                                validationMethod: (value) {
+                                  return bloc.passwordValidation(text: value);
+                                },
+                              ),
+                            ],
+                          ),
+                        );
+                      },
+                    ),
+                    CustomText(
+                      firstText: "Don't have an account? ",
+                      secondText: " Reigster",
+                      destination: RegisterScreen(),
+                    ),
 
-                  Spacer(),
-                  SubmitButton(
-                    buttonLabel: "Login",
-                    pressedFunction: () {
-                      bloc.add(LoginEvent());
-                    },
-                    isEnabled: bloc.isEnabledButton,
-                  ),
+                    Spacer(),
+                    SubmitButton(
+                      buttonLabel: "Login",
+                      pressedFunction: () {
+                        bloc.add(LoginEvent());
+                      },
+                      isEnabled: bloc.isEnabledButton,
+                    ),
 
-                  SizedBox(height: 64),
-                ],
+                    SizedBox(height: 64),
+                  ],
+                ),
               ),
             ),
           );
